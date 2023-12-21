1d34b1ada9cbbbb3052eeb7079098b8fa47a3d82
lakshmi <lakshmi.rajamoorthy@gmail.com>
2023-12-20 21:34:24 -0500

    maven project

---
+++ src/main/java/com/example/demo/Controller/MessageController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+  @GetMapping("/message")
+  public String displayMessage(Model model) {
+
+    model.addAttribute("message", "Hello, World!");
+    return "messageView";
+  }
+}
